33eb72dfce0faff5019c9739fc37e1eb10507f69
Lee Sutton <leesutton1@gmail.com>
2018-05-24 10:11:10 -0700

    Publish bots for user only

--- imports/api/bots/server/publications.js
+++ imports/api/bots/server/publications.js
@@ -4,5 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import { Bots } from '/imports/api/bots/bots.js';
 
 Meteor.publish('bots.all', function () {
-    return Bots.find({});
+    if (this.userId)
+        return Bots.find({userId: this.userId});
+    this.ready();
 });
--- imports/api/bots/server/publications.test.js
+++ imports/api/bots/server/publications.test.js
@@ -1,25 +1,24 @@
-
-// Tests for the bots publications
 import { expect } from 'chai';
 import { Bots } from '/imports/api/bots/bots.js';
 import { PublicationCollector } from 'meteor/johanbrook:publication-collector';
 import '/imports/api/bots/server/publications.js';
 
 describe('links publications', function () {
-      beforeEach(function () {
-            Bots.remove({});
-            Bots.insert({
-                title: 'test bot',
-                description: 'Test describe goes here',
-            });
+    beforeEach(function () {
+        Bots.remove({});
+        Bots.insert({
+            title: 'test bot',
+            description: 'Test describe goes here',
+            userId: 1,
+        });
     });
 
     describe('bots.all', function () {
-        it('sends all bots', function (done) {
+        it('sends all bots for the logged in user', function (done) {
             const collector = new PublicationCollector();
+            collector.userId = 1;
             collector.collect('bots.all', (collections) => {
                 try {
-
                     expect(collections.bots).to.not.be.undefined;
                     expect(collections.bots).to.have.lengthOf(1);
                     done();
@@ -28,5 +27,39 @@ describe('links publications', function () {
                 }
             });
         });
+
+        it('Returns undefined if the user is not logged in', function(done) {
+            const collector = new PublicationCollector();
+            collector.collect('bots.all', (collections) => {
+                try {
+                    expect(collections.bots).to.be.undefined;
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+
+        it('Returns bots for the logged in user only', function(done) {
+            Bots.insert({
+                title: 'second bot',
+                description: 'description',
+                userId: 2
+            });
+            const collector = new PublicationCollector();
+            collector.userId = 1;
+            collector.collect('bots.all', (collections) => {
+                try {
+                    expect(collections.bots).to.have.lengthOf(1);
+                    expect(collections.bots[0]).to.have.property('title', 'test bot');
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+
+        });
+
+
     });
 });
