7d97bd31cf6d92abaa480b5191f43ab373e07c73
Lee Sutton <leesutton1@gmail.com>
2018-05-24 10:50:00 -0700

    Meteor methods require userId

--- imports/api/bots/methods.js
+++ imports/api/bots/methods.js
@@ -4,8 +4,11 @@ import { Bots } from '/imports/api/bots/bots.js';
 
 Meteor.methods({
     'bots.insert'(name, description) {
+        if (!this.userId) {
+            throw new Meteor.Error('User not logged in');
+        }
         check(name, String);
         check(description, Match.OneOf(String, null));
-        return Bots.insert({name, description});
+        return Bots.insert({name, description, userId: this.userId});
     }
 });
--- imports/api/bots/methods.test.js
+++ imports/api/bots/methods.test.js
@@ -6,21 +6,22 @@ import './methods.js';
 
 if (Meteor.isServer) {
     describe('bots methods', function () {
+        const addBot = Meteor.server.method_handlers['bots.insert'],
+            dummyUser = {userId: 1};
+
         beforeEach(function () {
             Bots.remove({});
         });
 
         it('can add a new bot', function () {
-            const addBot = Meteor.server.method_handlers['bots.insert'];
-            let returnValue = addBot.apply({}, ['New Bot', 'Dummy description']);
+            let returnValue = addBot.apply(dummyUser, ['New Bot', 'Dummy description']);
 
             expect(Bots.find().fetch()).to.have.lengthOf(1);
             expect(returnValue).to.not.be.undefined;
         });
         it('Checks the name argument has the correct type', function() {
-            const addBot = Meteor.server.method_handlers['bots.insert'];
             let invalidCall = () => {
-                addBot.apply({}, [19, 'Dummy description']);
+                addBot.apply(dummyUser, [19, 'Dummy description']);
             };
 
             expect(invalidCall).to.throw('Expected string');
@@ -28,14 +29,27 @@ if (Meteor.isServer) {
         });
 
         it('Checks the description argument has the correct type', function() {
-            const addBot = Meteor.server.method_handlers['bots.insert'];
             let invalidCall = () => {
-                addBot.apply({}, ['Name here', 20]);
+                addBot.apply(dummyUser, ['Name here', 20]);
             };
 
             expect(invalidCall).to.throw();
             expect(Bots.find().fetch()).to.have.lengthOf(0);
         });
+
+        it('Stores the user id with the bot', function() {
+            addBot.apply(dummyUser, ['Name here', 'description']);
+            expect(Bots.find().fetch()).to.have.lengthOf(1);
+            expect(Bots.find().fetch()[0]).to.have.property('userId');
+        });
+
+        it('Throws if the user is not logged in', function() {
+            let invalidCall = () => {
+                addBot.apply({}, ['test', 'test']);
+            };
+            expect(invalidCall).to.throw();
+        });
+
 
     });
 }
