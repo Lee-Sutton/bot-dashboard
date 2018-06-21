eee9b889d02180c5f97086a7eed996a6bb06a233
Lee Sutton <leesutton1@gmail.com>
2018-06-20 20:31:52 -0700

    Cleaned up fixtures

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,11 +1,3 @@
-// Fill the DB with example data on startup
-//
-//
-// { "_id" : "P8uudqKmgqqc8wj2o", "createdAt" : ISODate("2018-05-20T20:36:15.680Z"), "services" : { "password" : { "bcrypt" : "$2b$10$Ru8
-// kQjC6pnCK1VCq0up7Ged8FOwcn8xEfBXDbc4aATYeXTOvaOPA6" }, "resume" : { "loginTokens" : [ { "when" : ISODate("2018-05-20T20:36:15.688Z"),
-// "hashedToken" : "holLpX3jiPzNMxK7c5VPizzUPS71ti3g1hWNW9iZk4U=" }, { "when" : ISODate("2018-05-24T01:08:53.794Z"), "hashedToken" : "CMm
-// PbdriiUr4bK8C2+1Wjhm808E5p9Iw6RWckGubEUg=" }, { "when" : ISODate("2018-05-24T01:50:12.538Z"), "hashedToken" : "A+LLiVWZrxDkwvPQYR+Fd5S
-// j+9BZr7QHO0S3AlmNnkY=" } ] } }, "emails" : [ { "address" : "leesutton1@gmail.com", "verified" : false } ] }
 
 import { Meteor } from 'meteor/meteor';
 
